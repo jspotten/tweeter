e95d872d6b05162def2c5f4988ecd95121f20698
Jaden <jespotten@hotmail.com>
2024-02-07 15:18:55 -0700

    Milestone 2A MVP Changes for StatusItemPresenter

--- tweeter-web/src/App.tsx
+++ tweeter-web/src/App.tsx
@@ -4,13 +4,15 @@ import Login from "./components/authentication/login/Login";
 import Register from "./components/authentication/register/Register";
 import MainLayout from "./components/mainLayout/MainLayout";
 import Toaster from "./components/toaster/Toaster";
-import {AuthToken, User, FakeData, Status} from "tweeter-shared";
 import UserItemScroller from "./components/mainLayout/UserItemScroller";
 import {StatusItemScroller} from "./components/mainLayout/StatusItemScroller";
 import {useUserInfoHook} from "./components/userInfo/UserInfoHook";
 import {UserItemView} from "./presenter/UserItemPresenter";
 import {FollowingPresenter} from "./presenter/FollowingPresenter";
 import {FollowersPresenter} from "./presenter/FollowersPresenter";
+import {StatusItemView} from "./presenter/StatusItemPresenter";
+import {FeedPresenter} from "./presenter/FeedPresenter";
+import {StoryPresenter} from "./presenter/StoryPresenter";
 
 const App = () => {
     const { currentUser, authToken } = useUserInfoHook();
@@ -34,26 +36,6 @@ const App = () => {
 };
 
 const AuthenticatedRoutes = () => {
-    const loadMoreFeedItems = async (
-        authToken: AuthToken,
-        user: User,
-        pageSize: number,
-        lastItem: Status | null
-    ): Promise<[Status[], boolean]> => {
-        // TODO: Replace with the result of calling server
-        return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
-    };
-
-    const loadMoreStoryItems = async (
-        authToken: AuthToken,
-        user: User,
-        pageSize: number,
-        lastItem: Status | null
-    ): Promise<[Status[], boolean]> => {
-        // TODO: Replace with the result of calling server
-        return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
-    };
-
     return (
         <Routes>
             <Route element={<MainLayout />}>
@@ -61,13 +43,21 @@ const AuthenticatedRoutes = () => {
             <Route
                 path="feed"
                 element={
-                    <StatusItemScroller loadItems={ loadMoreFeedItems } itemDescription={ "feed"} />
+                    <StatusItemScroller
+                        presenterGenerator={
+                            (view: StatusItemView) => new FeedPresenter(view)
+                        }
+                    />
                 }
             />
             <Route
                 path="story"
                 element={
-                    <StatusItemScroller loadItems={ loadMoreStoryItems } itemDescription={ "story"} />
+                    <StatusItemScroller
+                        presenterGenerator={
+                            (view: StatusItemView) => new StoryPresenter(view)
+                        }
+                    />
                 }
             />
             <Route
---
+++ tweeter-web/src/presenter/FeedPresenter.ts
@@ -0,0 +1,33 @@
+import {StatusService} from "../model/StatusService";
+import {StatusItemPresenter, StatusItemView} from "./StatusItemPresenter";
+import {AuthToken, Status, User} from "tweeter-shared";
+
+export const PAGE_SIZE = 10;
+
+export class FeedPresenter extends StatusItemPresenter{
+    private service: StatusService;
+    private lastItem: Status | null = null;
+
+    public constructor(view: StatusItemView) {
+        super(view);
+        this.service = new StatusService();
+    }
+
+    public async loadMoreItems(authToken: AuthToken, user: User)
+    {
+        try {
+            if (this.hasMoreItems)
+            {
+                let [newItems, hasMore] = await this.service.loadMoreFeedItems(authToken, user, PAGE_SIZE, this.lastItem);
+
+                this.hasMoreItems = hasMore;
+                this.lastItem = newItems[newItems.length - 1];
+                this.view.addItems(newItems);
+            }
+        } catch (error) {
+            this.view.displayErrorMessage(
+                `Failed to load feed because of exception: ${error}`
+            );
+        }
+    };
+}
