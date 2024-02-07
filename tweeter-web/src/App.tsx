9d3fe9717b0bab699b9176ecb028343fc701a0f9
Jaden <jespotten@hotmail.com>
2024-02-07 14:44:23 -0700

    Milestone 2A Setup Changes

--- tweeter-web/src/App.tsx
+++ tweeter-web/src/App.tsx
@@ -8,6 +8,9 @@ import {AuthToken, User, FakeData, Status} from "tweeter-shared";
 import UserItemScroller from "./components/mainLayout/UserItemScroller";
 import {StatusItemScroller} from "./components/mainLayout/StatusItemScroller";
 import {useUserInfoHook} from "./components/userInfo/UserInfoHook";
+import {UserItemView} from "./presenter/UserItemPresenter";
+import {FollowingPresenter} from "./presenter/FollowingPresenter";
+import {FollowersPresenter} from "./presenter/FollowersPresenter";
 
 const App = () => {
     const { currentUser, authToken } = useUserInfoHook();
@@ -50,25 +53,6 @@ const AuthenticatedRoutes = () => {
         // TODO: Replace with the result of calling server
         return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
     };
-    const loadMoreFollowers = async (
-        authToken: AuthToken,
-        user: User,
-        pageSize: number,
-        lastItem: User | null
-        ): Promise<[User[], boolean]> => {
-        // TODO: Replace with the result of calling server
-        return FakeData.instance.getPageOfUsers(lastItem, pageSize, user);
-    };
-
-    const loadMoreFollowees = async (
-        authToken: AuthToken,
-        user: User,
-        pageSize: number,
-        lastItem: User | null
-        ): Promise<[User[], boolean]> => {
-        // TODO: Replace with the result of calling server
-        return FakeData.instance.getPageOfUsers(lastItem, pageSize, user);
-    };
 
     return (
         <Routes>
@@ -90,8 +74,9 @@ const AuthenticatedRoutes = () => {
                 path="following"
                 element={
                     <UserItemScroller
-                        loadItems={loadMoreFollowees}
-                        itemDescription="followees"
+                        presenterGenerator={
+                            (view: UserItemView) => new FollowingPresenter(view)
+                        }
                     />
                 }
             />
@@ -99,8 +84,9 @@ const AuthenticatedRoutes = () => {
                 path="followers"
                 element={
                     <UserItemScroller
-                        loadItems={loadMoreFollowers}
-                        itemDescription="followers"
+                        presenterGenerator={
+                            (view: UserItemView) => new FollowersPresenter(view)
+                        }
                     />
                 }
             />
